a503eb8541246c91c7bda73e2eca5820dbc0e79b
Viki Wahyudi <47667838+VikiWahyudi05@users.noreply.github.com>
2020-04-30 07:39:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$('.efek').on('mouseover', function(){
+	$('.pKiri, .pK').addClass('pMuncul');
+});
+$('.efek').on('mouseleave', function(){
+	$('.pKiri, .pK').removeClass('pMuncul');
+});
+
+$('.efek1').on('mouseover', function(){
+	$('.pKiri1, .pK1').addClass('pMuncul');
+});
+$('.efek1').on('mouseleave', function(){
+	$('.pKiri1, .pK1').removeClass('pMuncul');
+});
+
+$('.efek2').on('mouseover', function(){
+	$('.pKiri2, .pK2').addClass('pMuncul');
+});
+$('.efek2').on('mouseleave', function(){
+	$('.pKiri2, .pK2').removeClass('pMuncul');
+});
+$('.efek3').on('mouseover', function(){
+	$('.pKiri3, .pK3').addClass('pMuncul');
+});
+$('.efek3').on('mouseleave', function(){
+	$('.pKiri3, .pK3').removeClass('pMuncul');
+});
